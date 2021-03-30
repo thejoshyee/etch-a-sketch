dcc0f3ee9340e327838bf8d4eba26473e94e9a8c
thejoshyee <69286716+thejoshyee@users.noreply.github.com>
2021-03-29 21:50:29 -0700

    updates to html, css, js

---
+++ js/app.js
@@ -0,0 +1,81 @@
+// Random RGB mouseover
+function randomRGB () {
+    while (true) {
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`; 
+    return rgb;
+    }
+}
+
+// Create Div grid
+const container = document.getElementById("container");
+function makeGrid(rows, cols) {
+    
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let gridItem = document.createElement("div");
+        gridItem.classList.add("gridItem");
+        gridItem.style.backgroundColor = 'white';
+        document.querySelector("#container").appendChild(gridItem);
+        gridItem.addEventListener("mouseover", function (e) {
+            gridItem.style.backgroundColor = randomRGB();
+        });
+    };
+};
+
+// Default Reset Setting
+document.onload = makeGrid(20, 20);
+
+//Reset Button - reset grid colors
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", function() {
+    const allDivs = document.querySelectorAll('.gridItem')
+    .forEach(e => e.style.backgroundColor = 'white'); 
+});
+
+
+//Slider to adjust size 
+
+var slider = document.getElementById("mySize");
+var output = document.getElementById("number");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    const newSize = slider.value;
+    output.innerHTML = this.value;
+    removeAllChildNodes(container);
+    makeGrid(newSize, newSize);
+}
+
+
+
+
+
+// Remove Child Nodes
+function removeAllChildNodes(parent) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+
+
+
+
+// Old Function for prompting size 
+// const changeSizeButton = document.querySelector(".changeSize");
+// changeSizeButton.addEventListener("click", function() {
+
+//     do{
+//         var selection = parseInt(window.prompt("Please enter a number from 1 to 100", ""), 10);
+//     }while(isNaN(selection) || selection > 100 || selection < 1);
+//     removeAllChildNodes(container);
+//     makeGrid(selection, selection);
+
+// });
